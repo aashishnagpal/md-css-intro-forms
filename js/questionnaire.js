98e3f61441946a93ddc2229bccad06aaf7869400
aashishnagpal <aashish.nagpal@hotmail.com>
2016-12-26 21:20:46 +0530

    Update Questionnaire Form: Added Validation rules

---
+++ js/questionnaire.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Aashish on 12/26/2016.
+ */
+
+(function () {
+  var txtInput = document.getElementById('text-other');
+  Array.prototype.forEach.call(document.getElementsByName('fav-person'), function (element) {
+    element.addEventListener('click', function (event) {
+
+      if (event.target.id === 'radio-other') {
+        txtInput.setAttribute('required', '');
+      } else {
+        txtInput.removeAttribute('required');
+      }
+    });
+  });
+
+  txtInput.addEventListener('keyup', function() {
+    if (!validator.isAlphanumeric(txtInput.value.replace(/ /g, ''))) {
+      txtInput.setCustomValidity('A person name can only contain alphanumeric characters.');
+    } else {
+      txtInput.setCustomValidity('');
+    }
+  });
+
+  document.getElementById('submit').addEventListener('click', function (event) {
+    document.getElementById('questionnaireForm').classList.add('validate');
+  });
+})();
